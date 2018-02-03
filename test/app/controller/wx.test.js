e851a1e68320bfc1dda34a0ea6ae818deb9e2651
TianYu <1127114376@qq.com>
2018-02-03 13:27:30 +0800

    This is the final one

---
+++ test/app/controller/wx.test.js
@@ -0,0 +1,16 @@
+const { app, assert } = require('egg-mock/bootstrap');
+
+describe('test/app/controller/home.test.js', () => {
+
+  it('should assert', function () {
+    const pkg = require('../../../package.json');
+    assert(app.config.keys.startsWith(pkg.name));
+  });
+
+  it('should GET /', () => {
+    return app.httpRequest()
+      .get('/wx')
+      .expect('Signature not match.')
+      .expect(400);
+  });
+});
